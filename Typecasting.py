c673495da2bb93cca2c17cfd42f3dfecb90ec19a
Yogita Manoj Damare <yogitadamare777@gmail.com>
2025-07-22 22:37:29 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,17 @@
+#Typecasting
+
+#Explict Typecasting
+
+string="15"
+number=10
+str_num=int(string)
+print(" the addition of ",str_num,"and",number,"is",str_num + number)
+
+# Implicit Typecasting
+a=9
+print(type(a))
+b=2.7
+print(type(b))
+c=a+b
+print("c=",c)
+print(type(c))
