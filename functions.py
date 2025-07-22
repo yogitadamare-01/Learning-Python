91e309ee438fc30c95210a4d55affbffa4a294b5
Yogita Manoj Damare <yogitadamare777@gmail.com>
2025-07-22 22:44:47 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+#Functions
+def calculate(a,b):
+    geomean=(a*b)/(a+b)
+    print(geomean)
+def isgreater(a,b):
+    if(a>b):
+        print("first number is greater ")
+    else:
+        print("second number is greater")
+def islesser(a,b):
+    pass
+a=3
+b=2
+calculate(a,b)
+isgreater(a,b)
+c=4
+d=7
+calculate(c,d)
+isgreater(c,d)
+calculate(8,12)
