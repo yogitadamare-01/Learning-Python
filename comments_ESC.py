6202537874e0042ca6d7aea79c746e153653ca1f
Yogita Manoj Damare <yogitadamare777@gmail.com>
2025-07-22 22:20:43 +0530

    Add files via upload

---
+++ comments_ESC.py
@@ -0,0 +1,17 @@
+#comment and Escape Sequence Character
+print("hi i am yogita")
+# i am using single line comment
+
+#multiline comment      
+""" i want to
+use multiline comment
+to use multiline comment triple double or
+single quotes are used"""
+
+#Escape Sequeunence character      
+print("i am yogita damare\nI am learning python\ni like to watch dramas")
+print("I am \"good girl\" My favourite food is pav-bhaji")
+
+#Separator and end
+print(6,4,7,8, sep= "-", end="numbers \n")
+print("hi")
