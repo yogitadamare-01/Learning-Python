c0e6daa69083412a6f22c1d2daec71069ca2959a
Yogita Manoj Damare <yogitadamare777@gmail.com>
2025-07-23 20:55:34 +0530

    Add files via upload

---
+++ Linear_Search.py
@@ -0,0 +1,14 @@
+#Linear Search
+customerid=[1001,1002,1003,1004,1005,1006,1007,1008,1009,1100]
+flag=0
+key=int(input("Enter the Customer Id"))
+for i in customerid:
+    if(key==i):
+        flag=1
+        break
+    else:
+        flag=0
+if(flag==1):
+    print("Customer Id is present")
+else:
+    print("Customer Id is not present")
